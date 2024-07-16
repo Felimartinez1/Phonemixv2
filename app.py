dfcec150a39e2ade78622953c7ce83f7b176317b
Felimartinez1 <felimartinez072@gmail.com>
2024-07-15 22:40:20 -0300

    change parameter

--- app.py
+++ app.py
@@ -24,9 +24,8 @@ async def home():
     return {"message": "Welcome to the Pronunciation Feedback API"}
 
 @app.post("/feedback/")
-async def pronunciation_feedback(file: UploadFile = File(...), expected_text: str = "", language: str = ""):
+async def pronunciation_feedback(file: UploadFile = File(...), expected_text: str = "", language: str = "es"):
     try:
-        print(file, expected_text, language) 
         # Read the content of the loaded file and convert it to an in-memory stream
         file_content = await file.read()
         user_audio_stream = BytesIO(file_content)
