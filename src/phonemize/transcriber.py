cc98838172278e00a4f998b83505fddc5f361180
Felimartinez1 <felimartinez072@gmail.com>
2024-07-17 22:23:06 -0300

    add audio preprocessing

--- src/phonemize/transcriber.py
+++ src/phonemize/transcriber.py
@@ -1,18 +1,33 @@
+import subprocess
+import os
 import speech_recognition as sr
 from config.config import phonemize_config
 
+def convert_audio(input_file, output_file):
+    """ Convert audio file to desired format using ffmpeg. """
+    command = ['ffmpeg', '-i', input_file, '-acodec', 'pcm_s16le', '-ar', '16000', '-ac', '1', output_file, '-y']
+    subprocess.run(command, check=True)
 
 def transcribe_audio(audio_file, phoneme_language=phonemize_config['language']):
     """ Transcribe audio to text using Google's speech recognition service, adjusting the language based on phoneme settings. """
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(audio_file) as source:
-        audio_data = recognizer.record(source)
-        try:
-            if phoneme_language == 'es':
-                return recognizer.recognize_google(audio_data, language='es-ES')
-            else:
-                return recognizer.recognize_google(audio_data)
-        except sr.UnknownValueError:
-            return "Google Speech Recognition could not understand audio"
-        except sr.RequestError as e:
-            return f"Could not request results from Google Speech Recognition service; {e}"
+    temp_file = '/tmp/temp_converted_audio.wav'
+    try:
+        # Convert the audio file
+        convert_audio(audio_file, temp_file)
+
+        recognizer = sr.Recognizer()
+        with sr.AudioFile(temp_file) as source:
+            audio_data = recognizer.record(source)
+            try:
+                if phoneme_language == 'es':
+                    return recognizer.recognize_google(audio_data, language='es-ES')
+                else:
+                    return recognizer.recognize_google(audio_data)
+            except sr.UnknownValueError as e:
+                return f"Google Speech Recognition could not understand audio: {e}"
+            except sr.RequestError as e:
+                return f"Could not request results from Google Speech Recognition service; {e}"
+    finally:
+        # Clean up temporary file
+        if os.path.exists(temp_file):
+            os.remove(temp_file)
